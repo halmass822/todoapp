f9c84d10a36472953dfdfc69583573a8069cbd9f
Hazem A <halmassri822@gmail.com>
2022-08-17 02:08:11 -0400

    app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [allTasks, setAllTasks] = useState([]);
+  const [newTask, setNewTask] = useState({});
+
+  const handleChange = ({target}) => {
+    const { name, value } = target;
+    setNewTask((prev) => {
+      return ({ ...prev, id: Date.now(), [name]: value });
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTask.taskName) return;  
+    setAllTasks((prev) => {
+      return ([newTask, ...prev]);
+    })
+    setNewTask({});
+  }
+
+  const handleRemove = (idToRemove) => {
+    setAllTasks((prev) => {
+      return prev.filter((i) => i.id !== idToRemove)
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="siteTitle">Todo App</h2>
+      <form className="inputSection" id="inputForm" onSubmit={handleSubmit}>
+        <div className="inputs">
+          <input id="inputTaskName" name="taskName" onChange={handleChange} value={newTask.taskName || ""} ></input>
+          <textarea id="inputTaskDescription" name="taskDescription" onChange={handleChange} value={newTask.taskDescription || ""} ></textarea>
+        </div>
+        <button className="submitButton" type="submit" form="inputForm" >Submit</button>
+      </form>
+      <div className="taskList">
+        {allTasks.map((task) => (
+          <div id={task.id} className="task" key={task.id}>
+            <h2 className="taskTitle"><button className="removeTask" onClick={() => handleRemove(task.id)}>X</button>{task.taskName}</h2>
+            <hr></hr>
+            <h2 className="taskDesc">{task.taskDescription}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
